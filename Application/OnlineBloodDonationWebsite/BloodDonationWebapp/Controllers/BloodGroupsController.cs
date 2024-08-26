38663ca5970fe38a3ecc43b5b3530be95ee9f060
ArvaAnkleshwaria <arvahabib1904@gmail.com>
2024-08-26 12:55:21 +0530

    initial commit.

---
+++ Application/OnlineBloodDonationWebsite/BloodDonationWebapp/Controllers/BloodGroupsController.cs
@@ -0,0 +1,108 @@
+﻿using BloodDonationWebapp.Models;
+using DatabaseLayer;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BloodDonationWebapp.Controllers
+{
+    public class BloodGroupsController : Controller
+    {
+        OnlineBloodDonation_DbEntities DB = new OnlineBloodDonation_DbEntities();
+        public ActionResult AllBloodGroupsType()
+        {
+            var bloodgroups = DB.BloodGroupsTables.ToList();
+            var listbloodgroups = new List<BloodGroupsMV>();
+            foreach (var bloodgroup in bloodgroups)
+            {
+                var addbloodgroup = new BloodGroupsMV();
+                addbloodgroup.BloodGroupID = bloodgroup.BloodGroupID;
+                addbloodgroup.BloodGroup = bloodgroup.BloodGroup;
+                listbloodgroups.Add(addbloodgroup);
+            }
+            return View(listbloodgroups);
+        }
+
+        public ActionResult Create()
+        {
+            var bloodgroup = new BloodGroupsMV();
+            return View(bloodgroup);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(BloodGroupsMV bloodgroupsMV)
+        {
+            if (ModelState.IsValid)
+            {
+                var bloodGroupsTable = new BloodGroupsTable();
+                bloodGroupsTable.BloodGroupID =bloodgroupsMV.BloodGroupID;
+                bloodGroupsTable.BloodGroup =bloodgroupsMV.BloodGroup;
+                DB.BloodGroupsTables.Add(bloodGroupsTable);
+                DB.SaveChanges();
+                return RedirectToAction("AllBloodGroupsType");
+            }
+            return View(bloodgroupsMV);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            var bloodgroup = DB.BloodGroupsTables.Find(id);
+            if (bloodgroup == null)
+            {
+                return HttpNotFound();
+            }
+            var bloodgroupsmv = new BloodGroupsMV();
+            bloodgroupsmv.BloodGroupID = bloodgroup.BloodGroupID;
+            bloodgroupsmv.BloodGroup = bloodgroup.BloodGroup;
+            return View(bloodgroupsmv);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(BloodGroupsMV bloodgroupsMV)
+        {
+            if (ModelState.IsValid)
+            {
+                var bloodGroupsTable = new BloodGroupsTable();
+                bloodGroupsTable.BloodGroupID = bloodgroupsMV.BloodGroupID;
+                bloodGroupsTable.BloodGroup = bloodgroupsMV.BloodGroup;
+
+                DB.Entry(bloodGroupsTable).State = EntityState.Modified;
+                DB.SaveChanges();
+                return RedirectToAction("AllBloodGroupsType");
+
+            }
+            return View(bloodgroupsMV);
+
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var bloodgroup = DB.BloodGroupsTables.Find(id);
+            if (bloodgroup == null)
+            {
+                return HttpNotFound();
+            }
+            var bloodgroupsmv = new BloodGroupsMV();
+            bloodgroupsmv.BloodGroupID = bloodgroup.BloodGroupID;
+            bloodgroupsmv.BloodGroup = bloodgroup.BloodGroup;
+            return View(bloodgroupsmv);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirm(int? id)
+        {
+            var bloodgroup = DB.BloodGroupsTables.Find(id);
+            DB.BloodGroupsTables.Remove(bloodgroup);
+            DB.SaveChanges();
+            return RedirectToAction("AllBloodGroupsType");
+        }
+    }
+}
---
+++ Application/OnlineBloodDonationWebsite/BloodDonationWebapp/Controllers/UserTypeController.cs
@@ -0,0 +1,106 @@
+﻿using BloodDonationWebapp.Models;
+using DatabaseLayer;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BloodDonationWebapp.Controllers
+{
+    public class UserTypeController : Controller
+    {
+        OnlineBloodDonation_DbEntities DB = new OnlineBloodDonation_DbEntities();
+        public ActionResult AllUserType()
+        {
+            var usertypes = DB.UserTypeTables.ToList();
+            var listusertype = new List<UserTypeMV>();
+            foreach (var usertype in usertypes)
+            {
+                var addusertype = new UserTypeMV();
+                addusertype.UserTypeID = usertype.UserTypeID;
+                addusertype.UseType = usertype.UseType;
+                listusertype.Add(addusertype);
+            }
+            return View(listusertype);
+        }
+        public ActionResult Create()
+        {
+            var usertype = new UserTypeMV();
+            return View(usertype);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(UserTypeMV userTypeMV)
+        {
+            if (ModelState.IsValid)
+            {
+                var userTypeTable = new UserTypeTable();
+                userTypeTable.UserTypeID = userTypeMV.UserTypeID;
+                userTypeTable.UseType = userTypeMV.UseType;
+                DB.UserTypeTables.Add(userTypeTable);
+                DB.SaveChanges();
+                return RedirectToAction("AllUserType");
+            }
+            return View(userTypeMV);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            var usertype = DB.UserTypeTables.Find(id);
+            if (usertype == null)
+            {
+                return HttpNotFound();
+            }
+            var usertypemv = new UserTypeMV();
+            usertypemv.UserTypeID = usertype.UserTypeID;
+            usertypemv.UseType = usertype.UseType;
+            return View(usertypemv);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(UserTypeMV userTypeMV)
+        {
+            if (ModelState.IsValid)
+            {
+                var userTypeTable = new UserTypeTable();
+                userTypeTable.UserTypeID = userTypeMV.UserTypeID;
+                userTypeTable.UseType = userTypeMV.UseType;
+
+                DB.Entry(userTypeTable).State = EntityState.Modified;
+                DB.SaveChanges();
+                return RedirectToAction("AllUserType");
+
+            }
+            return View(userTypeMV);
+
+        }
+        public ActionResult Delete(int? id)
+        { 
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var usertype = DB.UserTypeTables.Find(id);
+            if (usertype == null)
+            {
+                return HttpNotFound();
+            }
+            var usertypemv = new UserTypeMV();
+            usertypemv.UserTypeID = usertype.UserTypeID;
+            usertypemv.UseType = usertype.UseType;
+            return View(usertypemv);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirm(int? id)
+        {
+            var usertype = DB.UserTypeTables.Find(id);
+            DB.UserTypeTables.Remove(usertype);
+            DB.SaveChanges();
+            return RedirectToAction("AllUserType");
+        }
+    }
+}
